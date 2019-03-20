4754cd1298a9f6c3cb675ee964119cb40c987327
Werik Gonçalves <werikgpaula@gmail.com>
2019-03-20 20:10:05 -0300

    implementacao do vue js

--- frontend/js/app.js
+++ frontend/js/app.js
@@ -27,8 +27,9 @@ function login(){
             localStorage.setItem('access_token', result.access_token);
             localStorage.setItem('expires_at', result.expires_at);
             localStorage.setItem('status', result.status);
-            localStorage.setItem('token_type', result.token_type);    
-            window.location.href = "index.html"
+            localStorage.setItem('token_type', result.token_type);  
+            localStorage.setItem('league_name', result.league_name);  
+            window.location.href = "index.html";
         },
         error: function(data,status,er) {
             var error = JSON.parse(data.responseText);
@@ -51,11 +52,7 @@ function register(){
             password_confirmation: $('#password_confirmation').val()
         }),
         success: function(result) {
-            localStorage.setItem('access_token', result.access_token);
-            localStorage.setItem('expires_at', result.expires_at);
-            localStorage.setItem('status', result.status);
-            localStorage.setItem('token_type', result.token_type);    
-            window.location.href = "index.html"
+            window.location.href = "login.html";
         },
         error: function(data,status,er) {
             var error = JSON.parse(data.responseText);
@@ -64,3 +61,50 @@ function register(){
         }
     });
 }
+
+function getInfoUser(){
+    $.ajax({
+        type: "GET",
+        url: "http://localhost:8000/home/getUserInfo/",
+        contentType: 'application/json',
+        data: JSON.stringify({
+            league_name: app.league_name
+        }),
+        success: function(result) {
+            var resultado = result;
+        },
+        error: function(data,status,er) {
+            var error = JSON.parse(data.responseText);
+            alert(error.message);
+            console.log(data);
+        }
+    });
+}
+
+
+
+function verificaLogin(){
+    if(!(localStorage.getItem('access_token'))){
+        window.location.href = 'login.html';
+    }else{
+        app.league_name = localStorage.getItem('league_name');
+    }
+}
+
+var app = new Vue({
+    el: '#app',
+    data:{
+        league_name: ''
+    },
+    methods:{
+        setButtonA: _=>{
+            if(app.achou){
+                app.acaoMessage = "Revelar Mensagem"
+                app.achou = false;
+            }else{
+                app.acaoMessage = "Esconder Mensagem"
+                app.achou = true;
+            }
+        }
+    }
+});
